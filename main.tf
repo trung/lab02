c6a497cc6576db39906cfbada1ec2a3fc0a6e7db
trung <trung@mdkt.org>
2017-10-12 12:10:47 -0400

    ec

--- main.tf
+++ main.tf
@@ -16,6 +16,30 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  vpc_id = "vpc-cda264b5"
+}
+
+
 data "aws_availability_zones" "all" {}
 
 output "myvar" {
